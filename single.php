f8e7be436ee28b51395b67e5913a17d4e5423690
Cheyne <chekle@users.noreply.github.com>
2023-07-04 19:19:33 +1200

    single post additions - featured image etc

--- single.php
+++ single.php
@@ -16,16 +16,64 @@ get_header();
 					while (have_posts()) : the_post(); ?>
 
 					<article id="blog-post" itemscope="" itemtype="http://schema.org/BlogPosting">
-						<div class="row">
+						<div class="row g-5">
 							<div class="col-12 col-lg-8">
-								<h1 itemprop="name headline"><?php the_title(); ?></h1>
+								<h1 itemprop="name headline" class="fw-bold"><?php the_title(); ?></h1>
+								<?php if ( get_field('subheading') ) : ?>
+									<h5 class="mb-0"><?php echo get_field('subheading'); ?></h5>
+								<?php endif; ?>
+							</div>
+							<div class="col-12 col-lg-4 d-flex align-items-end justify-content-end">
+								<div class="inner">
+									<i class="fa-regular fa-clock me-2"></i><?php echo do_shortcode('[rt_reading_time postfix="mins"]'); ?>
+								</div>
 							</div>
 						</div>
+
+						<?php if ( has_post_thumbnail()) : ?>
+							<div class="margin-sm">
+								<div class="row">
+									<div class="col-12">
+										<figure>
+											<?php the_post_thumbnail(); ?>
+										</figure>
+									</div>
+								</div>
+							</div>
+						<?php endif; ?>
 						
-						<div class="content" itemprop="articleBody"><?php the_content(); ?></div>
+						<div class="margin-sm">
+							<div class="row g-5">
+								<div class="col-12 col-lg-8">
+									<div class="content" itemprop="articleBody">
+										<?php the_content(); ?>
+									</div>
+								</div>
+								<aside class="col-12 col-lg-4 ps-5">
+									<div class="inner">
+										<h5 class="mb-3">Share this</h5>
+										<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>&quote=<?php the_title(); ?>" target="_blank" class="me-3"><i class="fa-brands fa-facebook"></i></a>
+										<a href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&url=<?php the_permalink(); ?>" target="_blank" class="me-3"><i class="fa-brands fa-twitter"></i></a>
+										<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>&summary=&source=" target="_blank" class="me-3"><i class="fa-brands fa-linkedin"></i></a>
+										<a href="mailto:?subject=<?php the_title(); ?>&body=<?php the_permalink(); ?>" target="_blank" class="me-3"><i class="fa-solid fa-envelope"></i></a>
+									</div>
+								</aside>
+							</div>
+						</div>
 
 						<div class="border-top">
-							<time datetime="<?php the_time('F jS, Y') ?>" itemprop="datePublished"><small><?php the_time('F jS, Y') ?></small></time>
+							<div class="padding-sm">
+								<div class="row g-5">
+									<div class="col-12 col-lg-4">
+										<p class="mb-4">Published:</p>
+										<h5><time datetime="<?php the_time('jS F, Y') ?>" itemprop="datePublished"><small><?php the_time('jS F, Y') ?></small></time></h5>
+									</div>
+									<div class="col-12 col-lg-4">
+										<p class="mb-4">Was this post helpful?</p>
+										<?php echo do_shortcode('[posts_like_dislike]');?>
+									</div>
+								</div>
+							</div>
 						</div>
 
 					</article>
